6ab04862b8ffe653a247b4b0da4c488c19a14722
inaidE <iperminov09@gmail.com>
2026-04-18 12:59:03 +0300

    init frontend with vite, tailwind and project structure

---
+++ datum_frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+      <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-slate-900">
+          React + Tailwind работает
+        </h1>
+      </div>
+  );
+}
